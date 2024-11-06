ea3b81851a279c67e958d592f4f71932eb1ed09d
Elle Nash <56604081+v0idx@users.noreply.github.com>
2024-11-06 19:08:38 +0000

    Reverted Next Config Changes For Now

--- next.config.js
+++ next.config.js
@@ -1,34 +1,5 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  async rewrites() {
-      return {
-        beforeFiles: [
-          {
-            source: '/',
-            has: [
-              {
-                type: 'host',
-                value: 'words.v0idx.com',
-              },
-            ],
-            destination: 'https://v0idx.com/words',
-          },
-          {
-            source: '/',
-            has: [
-              {
-                type: 'host',
-                value: 'words.localhost:3000',
-              }
-            ],
-            destination: 'http://localhost:3000/words',
-          },
-        ]
-      }
-  }
-}
+module.exports = nextConfig
